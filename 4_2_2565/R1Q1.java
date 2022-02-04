3a018eb5b3a3fdcadbd61326e21f4c1ffd726c5b
ktyjapz <88848316+KatanyooThanoosin@users.noreply.github.com>
2022-02-04 16:24:21 +0700

    Update R1Q1.java

--- 4_2_2565/R1Q1.java
+++ 4_2_2565/R1Q1.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class R1Test1 {
+public class R1Q1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Input size of matrix: ");
